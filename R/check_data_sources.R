5ff8794ef2fe369630058ade5caa2567aeaa0e49
Andrea Manica <am315@cam.ac.uk>
2022-11-09 14:47:24 +0000

    First draft of checking function

---
+++ R/check_data_sources.R
@@ -0,0 +1,39 @@
+#' Check that data_sources are complete
+#'
+#' Check that all directories in /data/raw and /data/intermediate are found in data_sources.csv,
+#' and that their details are complete.
+#'
+#' @param path option string giving the path for the root of the repository (NOT the data
+#' directory). If left NULL, then the current working directory is used.
+#'
+#' @export
+
+
+check_data_sources <- function(path=NULL){
+  if (is.null(path)){
+    path = getwd()
+  }
+  if(!file.exists(file.path(path,"/data/data_sources.csv"))){
+    stop("data_sources.csv can not be found; are you in the root directory of your repository?")
+  }
+  
+  data_sources <- utils::read.csv (file.path(path,"/data/data_sources.csv"))
+  # check that there are two columsn with correct names
+  if (!all(c("directory","source","url") %in% names(data_sources))){
+    "data_sources.csv does not include the three mandatory columns: 'directory', 'sources' and 'url'"
+  }
+  if (any(is.na(data_sources$source), is.na(data_sources$url))){
+    stop("in data_sources.csv, some sources/url are have been left blank; \n",
+         "all entries should be filled in, use 'local' in both source and url for dirs in which files that are synchronised with git")
+  }
+  raw_dirs <- list.dirs(file.path(path,"data/raw"), full.names = FALSE, recursive=FALSE)
+  raw_dirs <- paste0("/data/raw/",raw_dirs)
+  intermediate_dirs <- list.dirs(file.path(path,"data/intermediate"), full.names = FALSE, recursive=FALSE)
+  if (length(intermediate_dirs)>0){
+    intermediate_dirs <- paste0("/data/intermediate/",intermediate_dirs)
+  }
+  if (!all(c(raw_dirs,intermediate_dirs) %in% data_sources$directory)){
+    stop("some directories are not listed in data_sources.csv")
+  }
+  return(TRUE)
+}
---
+++ inst/temp_ideas/some_ideas.R
@@ -0,0 +1,17 @@
+
+# list all files in data:
+data_dir<-"./"
+list.files(data_dir, recursive = TRUE)
+
+# check that the data directory does include valid directories
+
+
+
+# example workflow
+
+get_data_path("raw/penguin_tracks")
+set_data_dir
+
+list_data_remotes
+get_data_remote
+set_data_remote
