0cc25f16389ddfc0e9701764ec842d22f4a16ad8
Andrea Manica <am315@cam.ac.uk>
2022-11-22 12:58:34 +0000

    Add gitignore to template

--- R/init_resrepo.R
+++ R/init_resrepo.R
@@ -10,7 +10,8 @@
 init_resrepo <- function (path=".") {
   git_root <- find_git_root()
   template_dir <- system.file("template",package="resrepo")
-  copy_results <- file.copy(from=dir(template_dir,full.names = TRUE),
+  copy_results <- file.copy(from=dir(template_dir,full.names = TRUE,
+                                     all.files=TRUE),
             to = git_root, recursive = TRUE, overwrite = TRUE)
   if (all(copy_results)){
     return(TRUE)
