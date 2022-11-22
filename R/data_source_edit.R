5060bc442f0a0922046a537273996346f6e5ceb1
James Kennerley <jameskennerley@rocketmail.com>
2022-11-22 17:59:08 +0000

    Minor tweaks.

--- R/data_source_edit.R
+++ R/data_source_edit.R
@@ -25,7 +25,7 @@ data_sources_edit<- function(path=NULL, directory=NULL, new_directory = NULL, ne
   original_sources <- read.csv("./data/data_sources.csv")
   
   if(!(directory %in% original_sources$directory)){
-    temp_sources<-data.frame('directory' =NA, 'source' = NA, 'url' = NA, 'notes'=NA)
+    temp_sources<-data.frame('directory' = NA, 'source' = NA, 'url' = NA, 'notes' = NA)
     temp_sources$directory<-directory
     temp_sources$source <- source
     temp_sources$url <- url
@@ -36,7 +36,7 @@ data_sources_edit<- function(path=NULL, directory=NULL, new_directory = NULL, ne
   write.csv(new_sources, "./data/data_sources.csv",row.names = FALSE)
   
   data_sources <- utils::read.csv (file.path(path,"/data/data_sources.csv"))
-  # check that there are two columsn with correct names
+  # check that the three mandatory columns are present with correct names
   if (!all(c("directory","source","url") %in% names(data_sources))){
     "data_sources.csv does not include the three mandatory columns: 'directory', 'source' and 'url'"
   }
@@ -47,9 +47,11 @@ data_sources_edit<- function(path=NULL, directory=NULL, new_directory = NULL, ne
   raw_dirs <- list.dirs(file.path(path,"data/raw"), full.names = FALSE, recursive=FALSE)
   raw_dirs <- paste0("/data/raw/",raw_dirs)
   intermediate_dirs <- list.dirs(file.path(path,"data/intermediate"), full.names = FALSE, recursive=FALSE)
+  
   if (length(intermediate_dirs)>0){
     intermediate_dirs <- paste0("/data/intermediate/",intermediate_dirs)
   }
+  
   if (!all(c(raw_dirs,intermediate_dirs) %in% data_sources$directory)){
     stop("some directories are not listed in data_sources.csv")
   }
