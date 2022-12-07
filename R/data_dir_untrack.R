52166e824ec64dd9b93526ac710ae5a98b5d88b0
Andrea Manica <am315@cam.ac.uk>
2022-12-07 06:30:21 +0000

    early tracking

--- R/data_dir_untrack.R
+++ R/data_dir_untrack.R
@@ -1,13 +1,13 @@
-#' Track a data directory with git
+#' Untrack a data directory with git
 #'
-#' This function sets up a data directory to be tracked 
+#' This function removes tracking for a data directory in git.
 #'
 #' @param data_path the path of the data directory, given as relative to the root
 #' of the git repository (e.g. "/data/raw/my_new_dir")
 #'
 #' @export
 
-data_dir_track <- function (path) {
+data_dir_untrack <- function (path) {
   if (!dir.exists(path_resrepo(path))){
     stop("the path ", path," does not exist!")
   }
