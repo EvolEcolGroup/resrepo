856a415ef1af384c5d060b73b6d5350a3c879d8f
OndraPelanek <ondrap2@gmail.com>
2025-06-03 15:59:49 +0100

    changing get_resrepo_git_creds.R to return correct git credentials for https

--- R/get_resprepo_git_creds.R
+++ R/get_resprepo_git_creds.R
@@ -18,7 +18,7 @@ get_resrepo_git_creds <- function(){
     )
     return(creds)
   }
-  git_url <- system("git config --get remote.origin.url", intern = TRUE)
+  # git_url <- system("git config --get remote.origin.url", intern = TRUE)
   if (grepl("^git@", git_url)) {
     # SSH URL
     creds <- list(
@@ -30,19 +30,27 @@ get_resrepo_git_creds <- function(){
     # copy to the PAT to an env variable
     # catch the error if we don't have one
     tryCatch(
-      git_host <- gitcreds::gitcreds_get()$host,
+      gitcreds::gitcreds_get()$host,
       error = function(e) {
         stop("You don't have a PAT set up with `gitcreds`. Please set it using gitcreds::gitcreds_set().")
       }
     )
     git_env_var <- gitcreds::gitcreds_cache_envvar(url = git_url)
+    github_pat_full = Sys.getenv(git_env_var)
+    github_pat_full <- strsplit(github_pat_full,":")
+    token <- github_pat_full[[1]][which(github_pat_full[[1]] == "password")+1]
+    username <- github_pat_full[[1]][which(github_pat_full[[1]] == "username")+1]
 
     # HTTPS URL
     creds <- list(
       type = "https",
       url = git_url,
-      github_pat = Sys.getenv(git_env_var)
+      github_pat = git2r::cred_user_pass(username = username,
+                                         password = token)
     )
+    
+
+    
   } else {
     stop("Unsupported Git URL format.")
   }
