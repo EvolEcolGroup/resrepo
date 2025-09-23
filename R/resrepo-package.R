64706ee7f19de37d15e0fe86d32896e77af2a905
Andrea Manica <am315@cam.ac.uk>
2025-09-23 11:51:01 +0100

    Update renv saving

---
+++ R/resrepo-package.R
@@ -0,0 +1,16 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+# dummy function to avoid R CMD check NOTE about renv
+dummy_renv <- function() {
+  renv::activate()
+}
+
+# dummy function to avoid R CMD check NOTE about studioapi
+dummy_studioapi <- function() {
+  studioapi::getActiveProject()
+}
